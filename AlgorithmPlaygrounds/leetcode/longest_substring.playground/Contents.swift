ef4df225c004ebe8d66ba07214ebee5022ffc89b
Pawel Rusin <rusin.paw@gmail.com>
2018-09-11 09:17:35 +0900

    leetcode: longest substring

--- AlgorithmPlaygrounds/leetcode/longest_substring.playground/Contents.swift
+++ AlgorithmPlaygrounds/leetcode/longest_substring.playground/Contents.swift
@@ -1,13 +1,37 @@
-Given a string, find the length of the longest substring without repeating characters.
+//Given a string, find the length of the longest substring without repeating characters.
+//
+//Examples:
+//
+//Given "abcabcbb", the answer is "abc", which the length is 3.
+//
+//Given "bbbbb", the answer is "b", with the length of 1.
+//
+//Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-Examples:
+import Foundation
 
-Given "abcabcbb", the answer is "abc", which the length is 3.
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
 
-Given "bbbbb", the answer is "b", with the length of 1.
+        var startIndex = s.startIndex
+        var longestSubstring = ""
+        var currentSubstring = ""
+        
+        for index in s.indices {
+            let currentSubstringIndex = s.index(startIndex, offsetBy: currentSubstring.count)
+            currentSubstring = String(s[startIndex...currentSubstringIndex])
+            if let previousPosition = currentSubstring.range(of: String(s[index])) {
+                // check for lengths, if its higher then the highest, use this one
+                if currentSubstring.count > longestSubstring.count {
+                    longestSubstring = currentSubstring
+                }
+                let substringToRemove = currentSubstring[...previousPosition.lowerBound]
+                startIndex =  s.index(startIndex, offsetBy: substringToRemove.count)
+            }
+        }
+        return longestSubstring.count
+    }
+}
 
-Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
-import UIKit
-
-var str = "Hello, playground"
+let s = Solution()
+s.lengthOfLongestSubstring("bbbbb")
