359ac8ee666dcaaa529f356e631e4d3604837fc6
Pawel Rusin <rusin.paw@gmail.com>
2018-10-01 23:16:44 +0900

    enough for today

--- graph_traversal.swift
+++ graph_traversal.swift
@@ -1,38 +1,69 @@
 import Foundation
 
 class Graph {
-  private var adjencyList: [[Node]] = [[Node]]()
+  private var adjencyList: [Int : [Node]] = [Int : [Node]]()
 
-  func addEdge(value: Int, value2: Int) {
-    var first = adjencyList.first(where: {
-      $0.label == value
-    })
+  func addEdge(_ label: Int, _ label2: Int) {
+    let node1 = Node(label: label)
+    let node2 = Node(label: label2)
 
-    var second = adjencyList.first(where: {
-      $0.label == value2
-    })
+    var firstN = adjencyList[label]
+    var secondN = adjencyList[label2]
 
-    if first != nil && second != nil {
-    // we have 
-
+    if firstN != nil {
+      firstN!.append(node2)
+      print("appended \(node2.label)")  
     } else {
-      
+      adjencyList[label] = [node2]
+      print("set \(node2.label)")  
+    }
+    if secondN != nil {
+      secondN!.append(node1)
+      print("appended \(node1.label)")  
+    } else {
+      adjencyList[label2] = [node1]
+      print("set \(node1.label)")  
     }
   }
 
   func traverseDepth() {
-
+    var (anyVertex, _) = adjencyList.randomElement()!
+    traverseDepth(node: adjencyList[anyVertex]!.first!)
   }
 
+  private func traverseDepth(node: Node) {
+  print("entering \(node.label)")
+  var neighbours = adjencyList[node.label]
+  if !node.visited {
+    node.visited = true
+    print("visited: \(node.label)")
+  }
+    for n in neighbours! {
+    print("looping \(n.label)")
+      if !n.visited {
+        traverseDepth(node: n)
+      }
+    }
+  }
 }
 
-class Node {
-  label: Int
-  visited: Bool
+class Node: Equatable {
+  let label: Int
+  var visited: Bool
 
   init(label: Int) {
     self.label = label
-    visited = false
+    self.visited = false
   }
 
+  static func == (lhs: Node, rhs: Node) -> Bool {
+        return lhs.label == rhs.label
+        }
 }
+
+
+var g = Graph()
+g.addEdge(1,3)
+g.addEdge(3,4)
+
+g.traverseDepth()
