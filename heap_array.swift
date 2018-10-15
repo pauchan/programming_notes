b747b3eb4a625cb670165deef38d96cb82f42640
Pawel Rusin <rusin.paw@gmail.com>
2018-10-15 22:12:19 +0900

    implement heap array

--- heap_array.swift
+++ heap_array.swift
@@ -8,21 +8,38 @@ class Heap {
     memory.append(value)
     let index = memory.count - 1
     let pIndex = parentIndex(index)
-    swap(memory: memory, child: memory.count - 1, parent: pIndex)
+    swap(child: memory.count - 1, parent: pIndex)
   }
 
-  func swap(memory: [Int], child: Int, parent: Int) {
-    if memory[child] > memory[parent] {
+  func swap(child: Int, parent: Int) {
+    // print("child: \(child) parent: \(parent)")
+    if parent >= 0 && memory[child] > memory[parent] {
       let temp = memory[child]
       memory[child] = memory[parent]
       memory[parent] = temp
-      swap(memory: memory, child: parent, parent: parentIndex(parent))
+      swap(child: parent, parent: parentIndex(parent))
     }
   }
 
-  func parentIndex(_ index: Int) -> Int {
-    return ceil((index / 2) as Int) - 1
+  func extract() -> Int {
+    let rValue = memory[0]
+    heapify(index: 0)
+    return rValue
   }
+
+  func heapify(index: Int) {
+    let l = leftIndex(index)
+    let r = rightIndex(index)
+    if r < memory.count
+  }
+
+  func parentIndex(_ index: Int) -> Int {
+    return Int(ceil(Double(index / 2))) - 1
+  }
+
+
+
+
 }
 
 let h = Heap()
@@ -31,5 +48,7 @@ h.add(5)
 h.add(12)
 h.add(1)
 h.add(11)
+h.add(32)
+h.add(15)
 print(h.memory)
 
