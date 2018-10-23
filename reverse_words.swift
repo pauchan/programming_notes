ec7c6e49a75cba3dbb5083d4e899f98d4df0f86b
Pawel Rusin <pawel-rusin@cookpad.com>
2018-10-23 10:27:44 +0900

    reverse words

---
+++ reverse_words.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func reverseWords(_ input: String) -> String {
+  var buffer = ""
+  var stack: [String] = []
+  input.forEach {
+    if $0 == " " {
+      stack.insert(buffer, at: 0)
+      buffer = ""
+    } else {
+      buffer.append($0)
+    }
+  }
+  stack.insert(buffer, at: 0)
+  return stack.reduce("", { (acc, el) in
+    acc + el + " "
+  })
+}
+
+
+print(reverseWords("Do or do not, there is no try."))
